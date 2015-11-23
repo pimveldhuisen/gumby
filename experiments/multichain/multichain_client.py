77e8f97a50dc39a6ecea07976496c72ecbdbe2ab
Steffan Norberhuis <steffan@norberhuis.nl>
2015-11-23 11:47:21 +0100

    MultiChain: synthetic experiments added.

--- experiments/multichain/multichain_client.py
+++ experiments/multichain/multichain_client.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 from os import path, environ
 from sys import path as pythonpath
-from time import sleep
 import base64
 
 from gumby.experiments.dispersyclient import DispersyExperimentScriptClient, main
 
 from twisted.python.log import msg
+from twisted.internet import reactor
 
 pythonpath.append(path.abspath(path.join(path.dirname(__file__), '..', '..', '..', "./tribler")))
 
 from Tribler.dispersy.candidate import Candidate
-from Tribler.community.multichain.community import MultiChainCommunity, MultiChainCommunityCrawler
+from Tribler.community.multichain.community import MultiChainCommunity, MultiChainCommunityCrawler, MultiChainScheduler
 
 
 class MultiChainClient(DispersyExperimentScriptClient):
@@ -19,11 +19,12 @@ class MultiChainClient(DispersyExperimentScriptClient):
     Gumby client to start the MultiChain Community
     """
 
-    def __init__(self, *argv, **kwargs):
-        DispersyExperimentScriptClient.__init__(self, *argv, **kwargs)
+    def __init__(self, *args, **kwargs):
+        DispersyExperimentScriptClient.__init__(self, *args, **kwargs)
         msg("Starting MultiChain client")
         # Set the default MultiChainCommunity as community
-        self.community_class = MultiChainCommunity
+        self.community_class = MultiChainGumbyCommunity
+        self.scheduler = None
         self.vars['public_key'] = base64.encodestring(self.my_member_key)
 
     def registerCallbacks(self):
@@ -31,9 +32,12 @@ class MultiChainClient(DispersyExperimentScriptClient):
         self.scenario_runner.register(self.set_multichain_type, 'set_multichain_type')
         self.scenario_runner.register(self.request_signature, 'request_signature')
         self.scenario_runner.register(self.request_block, 'request_block')
+        self.scenario_runner.register(self.increase_kbytes_sent, 'increase_kbytes_sent')
+        self.scenario_runner.register(self.increase_kbytes_received, 'increase_kbytes_received')
+        self.scenario_runner.register(self.schedule_block, 'schedule_block')
         self.scenario_runner.register(self.close, 'close')
 
-    def set_multichain_type(self, multichain_type='MultiChainCommunity'):
+    def set_multichain_type(self, multichain_type='MultiChainGumbyCommunity'):
         """
         Sets the community class of this gumby node to a special community.
         """
@@ -50,8 +54,10 @@ class MultiChainClient(DispersyExperimentScriptClient):
         else:
             raise RuntimeError("Tried to set to unknown community:%s." % multichain_type)
 
-    def online(self):
-        DispersyExperimentScriptClient.online(self)
+    def online(self, dont_empty=False):
+        DispersyExperimentScriptClient.online(self, dont_empty)
+        self.scheduler = MultiChainScheduler(self._community)
+        msg("%s: MID: %s" % (self.my_id, self._community.my_member.mid))
 
     def request_signature(self, candidate_id):
         msg("%s: Requesting Signature for candidate: %s" % (self.my_id, candidate_id))
@@ -66,6 +72,34 @@ class MultiChainClient(DispersyExperimentScriptClient):
         candidate = self._community.get_candidate((str(target['host']), target['port']))
         print("Candidate: %s" % candidate.get_member())
         self._community.publish_request_block_message(candidate, int(sequence_number))
+
+    def increase_kbytes_sent(self, candidate_id, amount_sent):
+        """
+        Increase the Kbytes received from a particular node in the MultiChainScheduler.
+        """
+        msg("%s: Increasing bytes sent: %s For candidate: %s" % (self.my_id, amount_sent, candidate_id))
+        target = self.all_vars[candidate_id]
+        peer = (str(target['host']), target['port'])
+        self.scheduler.update_amount_send(peer, int(amount_sent) * 1000)
+
+    def increase_kbytes_received(self, candidate_id, amount_received):
+        """
+        Increase the Kbytes received from a particular node in the MultiChainScheduler.
+        """
+        msg("%s: Increasing bytes received: %s For candidate: %s" % (self.my_id, amount_received, candidate_id))
+        target = self.all_vars[candidate_id]
+        peer = (str(target['host']), target['port'])
+        self.scheduler.update_amount_received(peer, int(amount_received)*1000)
+
+    def schedule_block(self, candidate_id):
+        """
+        Finish up the download.
+        :param candidate_id:
+        """
+        msg("%s: Scheduling block for candidate: %s" % (self.my_id, candidate_id))
+        target = self.all_vars[candidate_id]
+        peer = (str(target['host']), target['port'])
+        self.scheduler.schedule_block(peer)
 
     def introduce_candidates(self):
         """
@@ -85,7 +119,16 @@ class MultiChainClient(DispersyExperimentScriptClient):
         self._community.unload_community()
 
 
-class MultiChainDelayCommunity(MultiChainCommunity):
+class MultiChainGumbyCommunity(MultiChainCommunity):
+    """
+    Test community that does not remove candidates.
+    """
+
+    def cleanup_candidates(self):
+        return 0
+
+
+class MultiChainDelayCommunity(MultiChainGumbyCommunity):
     """
     Test Community that delays signature requests.
     """
@@ -99,13 +142,14 @@ class MultiChainDelayCommunity(MultiChainCommunity):
         Ignore the signature requests.
         :param message: the to be delayed request
         """
+        def continue_after_delay():
+            self.logger.info("Delay over.")
+            super(MultiChainDelayCommunity, self).allow_signature_request(message)
         self.logger.info("Received signature request that will delayed for %s." % self.delay)
-        sleep(self.delay)
-        self.logger.info("Delay over.")
-        super(MultiChainDelayCommunity, self).allow_signature_request(message)
+        reactor.callLater(self.delay, continue_after_delay)
 
 
-class MultiChainNoResponseCommunity(MultiChainCommunity):
+class MultiChainNoResponseCommunity(MultiChainGumbyCommunity):
     """
     Test Community that does not respond to signature requests.
     """
