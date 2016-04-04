2c8aaffaad385b7f2e8b1ff4b5af5a57d5055dcd
Captain-Coder <Captain-Coder@users.noreply.github.com>
2016-04-04 12:34:03 +0000

    Plot edge lables

--- experiments/multichain/r/generate_multichain_graph.r
+++ experiments/multichain/r/generate_multichain_graph.r
@@ -35,19 +35,19 @@ add_block_edges_to_graph <- function(block){
                 # Found Half-Signed Origin Block
              } else if (block["Previous_Hash_Responder"] == half_sign_hash_base64) {#or block.previous_hash_requester == GENESIS_ID:
                 # Half-Signed Block
-                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Requester"])))
+                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Requester"])), label=sprintf(block["id_req"]))
             } else if (block["Previous_Hash_Responder"] == genesis_id_base64 && block["Previous_Hash_Requester"] == genesis_id_base64) {
                 # Double Origin Block
             } else if (block["Previous_Hash_Requester"] == genesis_id_base64) {
                 # Found Requester Origin Block
-                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Responder"])))
+                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Responder"])), label=sprintf(block["id_resp"]))
             } else if (block["Previous_Hash_Responder"] == genesis_id_base64) {
                 # Found Responder Origin Block
-                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Requester"])))
+                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Requester"])), label=sprintf(block["id_req"]))
              } else {
                 #Normal block
-                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Responder"])))
-                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Requester"])))
+                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Responder"])), label=sprintf(block["id_resp"]))
+                graph<<-graph+edge(sprintf(block["Hash_Requester"]),sprintf(get_requester_hash_from_any_hash(block["Previous_Hash_Requester"])), label=sprintf(block["id_req"]))
             }
         },
         warning = function(w) {warning(w)},
@@ -82,21 +82,30 @@ if(file.exists("output/multichain.dat")) {
 if(multichain_file != "null") {
     print("Generating Multichain graph")
     data <- read.table(multichain_file, header = TRUE, sep =" ", row.names = NULL)
+    both <- union(levels(data$Public_Key_Requester), levels(data$Public_Key_Responder))
+    data <- cbind(id_req=as.numeric(factor(data$Public_Key_Requester, levels=both)), id_resp=as.numeric(factor(data$Public_Key_Responder, levels=both)), data)
+
     graph <- make_empty_graph()
 
     apply(data, 1, add_block_vertex_to_graph)
     apply(data, 1, add_block_edges_to_graph)
 
     svg('output/multichain.svg', width = 25, height = 25)
-    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.label=NA, vertex.color= V(graph)$color)
+    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.label=NA, vertex.color= V(graph)$color, edge.label=NA)
 
     svg('output/multichain_labels.svg', width = 25, height = 25)
-    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.color= V(graph)$color)
+    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.color= V(graph)$color, edge.label=NA)
 
-    png('output/multichain.png', width = 800, height = 800)
+    svg('output/multichain_edge_labels.svg', width = 25, height = 25)
     plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.label=NA, vertex.color= V(graph)$color)
 
+    png('output/multichain.png', width = 800, height = 800)
+    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.label=NA, vertex.color= V(graph)$color, edge.label=NA)
+
     png('output/multichain_labels.png', width = 800, height = 800)
-    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.color= V(graph)$color)
+    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.color= V(graph)$color, edge.label=NA)
+
+    png('output/multichain_edge_labels.png', width = 800, height = 800)
+    plot(graph, layout=layout.kamada.kawai, vertex.size=2, edge.color="black", vertex.label=NA, vertex.color= V(graph)$color)
 }
 
